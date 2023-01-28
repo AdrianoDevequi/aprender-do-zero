ab8d2e24e8f9f568e0bc08d031e88b1bbc2368a8
adrianodevequi <adrianodevequi@gmail.com>
2023-01-28 02:24:49 -0300

    Ajustes Index

--- index.php
+++ index.php
@@ -1,44 +1,313 @@
 <!doctype html>
 <html lang="pt-br">
-  <head>
-  <?php require_once '_head.php'; ?>
-  </head>
-  <body>
-    
-  <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    <h1>Hello, world!</h1>
-    
 
-    <?php require_once '_javascript.php'; ?>
-  </body>
+<head>
+	<?php require_once '_head.php'; ?>
+</head>
+
+<body>
+
+	<?php require_once '_header.php'; ?>
+
+	<section class="bg-black banner-home">
+		<div class="container pt-5">
+			<div class="row">
+				<div class="col-lg-12 text-center">
+					<h1 class="mb-4 text-white">A <span class="destaque text-black">tecnologia</span> já faz parte da rotina das pessoas</h1>
+					<p class="text-white">
+						Você consegue ficar um dia sem usar seu celular e interagir pelas redes sociais, como Youtube, Whatsapp ou Instagram? Pois bem, essas ferramentas de sucesso foram construídas por empreendedores que, através da tecnologia, criaram ferramentas que hoje são sucesso mundial e que valem bilhões. Um dia todos eles foram iniciantes: o segredo do sucesso é a perseverança.
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="bg-cinza" id="demanda-ti">
+		<div class="container pt-3 pb-0">
+			<div class="row align-items-center">
+				<div class="col-lg-6">
+					<h2 class="mb-4">Crie você também sua <span class="destaque">ferramenta TI de sucesso</span></h2>
+					<p>Já pensou <strong>você</strong>, que trabalha em uma área que não tem nada a ver com <strong>programação</strong>, nunca fez <strong>curso de programação</strong> ou ainda é <strong>iniciante em programação</strong>, mas pensa em empreender, desenvolvendo sua <strong>própria tecnologia</strong> no ramo no qual trabalha e lucrando pelo uso do seu sistema, vendido para o mundo inteiro?</p>
+				</div>
+				<div class="col-lg-6">
+					<img src="assets/imgs/estudante.png" alt="" class="img-fluid">
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="bg-black" id="casos-de-sucesso">
+		<div class="container pt-5 pb-5">
+			<div class="row">
+				<div class="col-lg-12 text-white">
+					<h1 class="mb-4 text-center">Casos de Sucesso</h1>
+					<p class="text-center pb-5">
+						Veja depoimentos de alguns alunos que fizeram sucesso com nosso curso de TI
+					</p>
+				</div>
+				<div class="col-lg-4">
+					<div class="text-center bg-cinza p-5 rounded mx-2 box-depoimento">
+						<h3>Arildo Mello</h3>
+						<p class="desc-projeto pb-2">Sistemas de Controle Financeiro, Emissão de Documentos, etc</p>
+						<p>Assim foi o que o guarda municipal Arildo Mello fez. Ele não tinha nenhum conhecimento de programação, mas tinha suas ideias de projeto. O que faltava para ele era somente conhecimento técnico. Então procurou nossa unidade física em Londrina para fazer o nosso curso de Programação para Iniciantes. O resultado foi o desenvolvimento dessas 2 ferramentas:</p>
+					</div>
+				</div>
+
+				<div class="col-lg-4">
+					<div class="text-center bg-cinza p-5 rounded mx-2 box-depoimento">
+						<h3>Depoimento</h3>
+						<p class="desc-projeto pb-2">Sistema ERP para pequenas e médias empresas</p>
+						<p>Outro caso de sucesso foi do agora empresário Vinicius Ricieri, que atualmente montou sua empresa com o sistema que criou de Gerenciamento Empresarial completo, sendo indicado para médias e pequenas empresas, o Dix Sistema.</p>
+					</div>
+				</div>
+
+				<div class="col-lg-4">
+					<div class="text-center bg-cinza p-5 rounded mx-2 box-depoimento">
+						<h3>O próximo caso de sucesso pode ser o seu!</h3>
+						<p class="desc-projeto pb-2">Sistemas de Controle Financeiro, Emissão de Documentos, etc</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec orci metus, elementum sollicitudin velit in, sollicitudin fermentum dui. Integer et mauris et ligula eleifend congue.</p>
+					</div>
+				</div>
+			</div>
+	</section>
+
+	<section class="bg-cinza">
+		<div class="container pt-5 pb-5">
+			<div class="row">
+				<div class="col-lg-12 text-center">
+					<h1 class="mb-4">O próximo caso de sucesso pode ser o seu!</h1>
+					<div class="row">
+						<div class="col-lg-3"><img src="assets/imgs/gl-1.jpg" class="rounded img-fluid" alt="..."></div>
+						<div class="col-lg-3"><img src="assets/imgs/gl-2.jpg" class="rounded img-fluid" alt="..."></div>
+						<div class="col-lg-3"><img src="assets/imgs/gl-3.jpg" class="rounded img-fluid" alt="..."></div>
+						<div class="col-lg-3"><img src="assets/imgs/gl-4.jpg" class="rounded img-fluid" alt="..."></div>
+					</div>
+					<p class="pt-4 pb-4">
+						Fora estes, temos vários outros casos de sucesso que nossa escola coleciona ao longo de seus 7 anos de existência! Sabemos o quanto que é difícil iniciar nesse ramo, porém temos o conhecimento, tecnologia e didática necessários para instruir nossos alunos da melhor forma possível.
+					</p>
+					<div class="row">
+						<div class="col-lg-4"><img src="assets/imgs/gl-1.jpg" class="rounded img-fluid" alt="..."></div>
+						<div class="col-lg-4"><img src="assets/imgs/gl-2.jpg" class="rounded img-fluid" alt="..."></div>
+						<div class="col-lg-4"><img src="assets/imgs/gl-3.jpg" class="rounded img-fluid" alt="..."></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="bg-black banner-sobre-treinamento" id="sobre-o-treinamento">
+		<div class="container pt-5 pb-5">
+			<div class="row">
+
+				<div class="col-lg-12 pb-5">
+					<h1 class="mb-4 text-white">Sobre o Treinamento Aprender do Zero</h1>
+				</div>
+
+				<div class="col-lg-6 text-white">
+
+					<h3 class="mb-4">Para quem é indicado?</h3>
+
+					<p>Nosso treinamento é voltado para pessoas que nunca viram <strong>programação</strong> e que querem mudar de profissão ou ter em paralelo uma outra profissão. Também é indicado para aquelas pessoas que já possuem um conhecimento básico, porém não têm a mínima ideia de como começar um projeto do zero.</p>
+
+					<p>Ideal para pessoas que queiram trabalhar na área TI, seja dentro de uma empresa ou trabalhando em serviços freelancers.</p>
+
+					<p>Como o treinamento não é só <strong>saber programar</strong>, mas sim <strong>analisar</strong>, montar o <strong>Banco de Dados</strong> de acordo a regra do sistema, <strong>comandos SQL</strong> e <strong>montar o front-end</strong> (telas do sistema), dividimos o curso em diversos módulos fundamentais para construção de um sistema de informação: <strong class="text-uppercase">análise, front-end, back-end e banco de dados</strong>.</p>
+
+					<p>Este curso de TI exige dedicação e compromisso do aluno, pois a cada semana será aprendido um novo conteúdo, no qual trabalharemos exercícios práticos em conjunto com a correção e acompanhamento tira-dúvidas <strong class="text-uppercase">ao vivo</strong> com o professor!</p>
+				</div>
+
+				<div class="col-lg-6 text-white" >
+					<h3 class="mb-4">O que vou aprender neste curso de TI?</h3>
+					<p class="xTxt">Para desenvolver um sistema de informação, temos diversas frentes para o uso do mesmo como por exemplo: <strong>desktop</strong>, que são programas instalados no PC ou notebook, e <strong>sistema para WEB</strong>, que pode ser acessado através de um Navegador como por exemplo, Google Chrome, Internet Explorer ou Mozilla.</p>
+                    
+					<p class="xTxt">Neste treinamento, você aprenderá a criar um sistema voltado para WEB, no qual poderá ser acessado tanto no computador, quanto em celulares ou tablets. A <strong>linguagem de programação</strong> que utilizaremos é o <strong>PHP</strong>, que é uma das linguagens de programação mais usadas no mercado mundial de desenvolvimento de sistemas Web.</p>
+
+					<p class="xTxt">Para abrangermos os 4 pilares de um desenvolvimento de sistemas: <strong class="text-uppercase">análise, front-end, back-end e banco de dados</strong>, o curso será dividido por módulos da seguinte maneira:</p>
+					</div>
+			</div>
+
+			<div class="row pt-5" id="o-que-vou-aprender">
+				<div class="col-lg-12">
+					<div class="accordion" id="accordionPerguntas">
+						<div class="accordion-item">
+							<h2 class="accordion-header" id="headingOne">
+								<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#p1" aria-expanded="true" aria-controls="p1">
+									Análise do sistema
+								</button>
+							</h2>
+							<div id="p1" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionPerguntas">
+								<div class="accordion-body">
+									<p>Neste módulo, iremos avaliar o projeto a ser construído - quais campos de cada tela, os cuidados que devemos ter com certas informações para desenvolver um sistema, etc.</p>
+								</div>
+							</div>
+						</div>
+						<div class="accordion-item">
+							<h2 class="accordion-header" id="headingTwo">
+								<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#p2" aria-expanded="false" aria-controls="p2">
+									Montagem do Front-End
+								</button>
+							</h2>
+							<div id="p2" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionPerguntas">
+								<div class="accordion-body">
+									<p>Junto com o módulo anterior, serão montadas todas as páginas HTMLs do projeto que será construído. Com o conhecimento adquirido neste módulo, o aluno conseguirá baixar templates bootstraps e montar qualquer layout para construção de sites dinâmicos (sites que envolvem programação) e estáticos (somente HTML)</p>
+								</div>
+							</div>
+						</div>
+						<div class="accordion-item">
+							<h2 class="accordion-header" id="headingThree">
+								<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#p3" aria-expanded="false" aria-controls="p3">
+									Lógica de programação Estruturada
+								</button>
+							</h2>
+							<div id="p3" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionPerguntas">
+								<div class="accordion-body">
+									<p>Pausaremos o projeto até então construído, para desenvolvermos a lógica de programação e conhecer as sintaxes da linguagem. Serão realizados exercícios explorando vários recursos da programação (for, if..else, switchcase, array, entre outros)</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="accordion-item">
+							<h2 class="accordion-header" id="headingThree">
+								<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#p4" aria-expanded="false" aria-controls="p4">
+									Lógica de programação e Orientação a objeto
+								</button>
+							</h2>
+							<div id="p4" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionPerguntas">
+								<div class="accordion-body">
+								<p>Dando a sequência à lógica de programação, neste módulo apresentaremos a programação orientada a objeto (POO).</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="accordion-item">
+							<h2 class="accordion-header" id="headingThree">
+								<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#p5" aria-expanded="false" aria-controls="p5">
+									JavaScript e JQuery
+								</button>
+							</h2>
+							<div id="p5" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionPerguntas">
+								<div class="accordion-body">
+									<p>A partir do momento que todas as páginas já foram criadas, neste módulo o aluno aprenderá a validar os campos obrigatórios através do JQuery/JavaScript. O mais bacana é: sabendo todos esses conceitos de lógica e utilizando uma linguagem de programação, quando for necessário migrar para uma outra linguagem de programação, a adaptação será bem mais fácil e ágil!</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="accordion-item">
+							<h2 class="accordion-header" id="headingThree">
+								<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#p6" aria-expanded="false" aria-controls="p6">
+									Banco de Dados: Modelagem do Banco de Dados (MySql) + SQL
+								</button>
+							</h2>
+							<div id="p6" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionPerguntas">
+								<div class="accordion-body">
+									<p>Neste módulo, retornaremos na construção de nosso projeto e construiremos o banco de dados do mesmo. E logo na sequencia, passaremos todas as instruções SQL do banco (programação dentro do banco de dados).</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="accordion-item">
+							<h2 class="accordion-header" id="headingThree">
+								<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#p7" aria-expanded="false" aria-controls="p7">
+									Conclusão do projeto
+								</button>
+							</h2>
+							<div id="p7" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionPerguntas">
+								<div class="accordion-body">
+									<p>Nessa última fase, aplicaremos os conceitos e práticas dos módulos de lógica para a finalização do projeto, através da programação orientada a objeto, onde criaremos as classes de comunicação com o Banco de Dados, interligando-as com as telas do sistema.</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="accordion-item">
+							<h2 class="accordion-header" id="headingThree">
+								<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#p8" aria-expanded="false" aria-controls="p8">
+									Publicação nas nuvens
+								</button>
+							</h2>
+							<div id="p8" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionPerguntas">
+								<div class="accordion-body">
+									<p>Aqui, o aluno aprenderá como contratar um domínio, que é o nome do site na internet (meu_sistema.com.br). Também fará a contratação de uma hospedagem, o que possibilitará deixar seu site já disponível para todos acessarem em qualquer lugar do mundo</p>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="bg-cinza" id="sobre-as-aulas">
+		<div class="container pt-5 pb-5">
+			
+			<div class="col-lg-12 pb-5">
+				<h1 class="mb-4">Sobre as aulas</h1>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-4">
+					<h4>Tempo de duração</h4>
+					<p>Nosso treinamento poderá ser concluído em torno de 5 meses, dependerá da performance de aprendizado do aluno.</p>       
+                    <p><strong>Importante:</strong> O mercado de trabalho exige portfólio, portanto, o projeto que construiremos em aula poderá ser o seu primeiro projeto. Ao apresentá-lo nas entrevistas de emprego, aumentará suas chances em ingressar rapidamente na área.</p>
+				</div>
+
+				<div class="col-lg-4">
+					<h4>Dinâmica das aulas</h4>
+					<p>A primeira aula gravada tem, no máximo, 40 minutos de duração e será disponibilizada ao aluno como uma preparação prática para a segunda aula <strong class="text-uppercase">ao vivo</strong> com o professor, para eventuais correções e momento tira-dúvidas dos alunos. Nesta segunda aula, há, além disso, a interação com os demais alunos. Com isso, ocorre a valiosa monitoria e mentoria diretamente com o professor, com duração de uma hora.</p>
+					<a href="#sobre-as-aulas" onclick="mostraTextoDinamica()">Ler Mais</a>
+					<div id="texto-dinamica" style="display: none;">
+						<p>Ao longo de toda nossa experiência, inclusive com nossos <strong>cursos online</strong>, constatamos que para aprender programação <strong>na maioria dos casos é necessário</strong> o acompanhamento com um profissional da área, por isso essa aula somada ao acompanhamento <strong class="text-uppercase">ao vivo</strong> agrega um valor significativo para o desenvolvimento do aprendizado.</p>
+						<p>Tivemos ao longo do tempo, muitos relatos de que a internet oferece muita coisa, porém nos cursos online que vemos por aí, faltam este acompanhamento profissional. Tempo é dinheiro, então é importante capacitar-se em uma escola que também possua uma unidade física, tem história e já transformou muitas carreiras de alunos que passaram por ela, como é o caso da <strong>WMBarros</strong>.</p>				
+					</div>
+				</div>
+	
+				<div class="col-lg-4">
+					<h4>Investimento</h4>
+					<p>Toda a abordagem apresentada do curso + as aulas ao vivo de acompanhamento com o professor semanalmente: 10 parcelas de <strong>R$ 198,00.</strong></p>                           
+                    <p><strong>Importante:</strong> só invista se realmente você está disposto(a) a dedicar-se ao curso! Queremos resultados, com alunos dispostos a fazer a diferença, com dedicação e compromisso!</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<script>
+		function mostraTextoDinamica() {
+		var x = document.getElementById("texto-dinamica");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+		}
+</script>
+	<section class="bg-black banner-footer" id="eu-quero">
+		<div class="container pt-5 pb-5 text-white">
+			<div class="row row justify-content-center">
+				<div class="col-lg-6">
+					<h1 class="text-center">Eu quero!</h1>
+					<p class="text-center pb-4">Matrículas abertas para as turmas de fevereiro. Preencha o formulário abaixo para fazer sua pré-matrícula e/ou tirar alguma dúvida</p>
+					<div class="row">
+						<div class="col-lg-12 mb-3">
+							<label class="form-label">Email address</label>
+							<input type="email" class="form-control" placeholder="name@example.com">
+						</div>
+						<div class="col-lg-12 mb-3">
+							<label class="form-label">Email address</label>
+							<input type="email" class="form-control" placeholder="name@example.com">
+						</div>
+						<div class="col-lg-12 mb-3">
+							<label class="form-label">Email address</label>
+							<input type="email" class="form-control" placeholder="name@example.com">
+						</div>
+						<div class="col-lg-12 mb-3">
+							<button type="button" class="btn btn-outline-success">Enviar</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<?php require_once '_javascript.php'; ?>
+</body>
+
 </html>
